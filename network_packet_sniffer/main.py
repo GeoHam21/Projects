959bdcedfbe8f2487f42952f61d3e166ac90de5d
GeoHam21 <geham21@gmail.com>
2025-01-28 23:55:35 +0000

    Moved and added code to make code easier to understand

--- network_packet_sniffer/main.py
+++ network_packet_sniffer/main.py
@@ -1,14 +1,5 @@
-from scapy.all import sniff
+import packet_sniffer
 
-#This is the callback function which will process the packets
-def callback(packet):
-    #This will print a short summary of the packet
-    print(packet.summary())
-
-
-#This will start sniffing on all the interfaces
-print("Sniffing on all network interfaces...")
-
-#This will capture 10 packets to process
-sniff(prn=callback, count=10)
+if __name__ == '__main__':
+    packet_sniffer.sniffing()
 
