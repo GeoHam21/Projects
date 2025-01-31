69c73d5504b581de6ea38849f4042cf87ffd85cc
GeoHam21 <geham21@gmail.com>
2025-01-31 23:30:57 +0000

    Added get_hostname function to add hostnames to IPs to make it more understandable

--- network_packet_sniffer/packet_sniffer.py
+++ network_packet_sniffer/packet_sniffer.py
@@ -1,19 +1,28 @@
+import socket
 from scapy.all import sniff
 from scapy.layers.inet import IP, UDP, TCP
 from scapy.layers.l2 import Ether
 
+def get_hostname(ip):
+    """Attempts to resolve an IP address to a hostname."""
+    try:
+        #This will get the hostname linked with the ip
+        hostname = socket.gethostbyaddr(ip)[0]
+        return hostname
+    except socket.herror:
+        return "Unknown Host"
+
+
 #This is the callback function which will process the packets
 def callback(packet):
+    src_ip = packet[IP].src
+    dst_ip = packet[IP].dst
+    src_host = get_hostname(src_ip)
+    dst_host = get_hostname(dst_ip)
+
     #This will print a short summary of the packet
     print("\n=== Packet Captured===")
-
-    #Check for ethernet layer
-    if Ether in packet:
-        print(f"Ethernet: {packet[Ether].src} -> {packet[Ether].dst}")
-
-    #Check for IP layer
-    if IP in packet:
-        print(f"IP: {packet[IP].src} -> {packet[IP].dst}")
+    print(f"Source: {src_ip} ({src_host}) -> Destination: {dst_ip} ({dst_host})")
 
     #Check for TCP layer
     if TCP in packet:
